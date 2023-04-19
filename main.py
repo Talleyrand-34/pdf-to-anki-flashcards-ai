01b39419780a3b44cc07366445816938ea6514d9
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2023-04-19 23:21:43 +0200

    large pdf to flashcard

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from PyPDF2 import PdfReader
 import openai
 import time
+import csv
+import json
+import subprocess
 
+with open("key.json") as f:
+    data = json.load(f)
 
+openai.api_key = data["openai_api_key"]
 
-openai.api_key = "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"
 pdf_path = "example.pdf"
 
 # Create a PdfReader object from the PDF file
@@ -14,27 +19,46 @@ reader = PdfReader(pdf_path)
 num_pages = len(reader.pages)
 
 # Initialize the prompt string with the CSV format instructions
-prompt = "Can you give me an answer with the structure of csv with the core ideas of this text in answer/question form? Only the CSV, do not add any other information as explanation or conclusion. Also, put it in plain text. Do not add a confirmation as 'sure' neither say 'in this example'. Also, at the beginning, do not add 'Question,Answer'."
-
+prompt_prefix = "Can you give me an answer with the structure of csv with the core ideas of this text in answer/question form? Only the CSV, do not add any other information as explanation or conclusion. Also, put it in plain text. Do not add a confirmation as 'sure' neither say 'in this example'. Also, at the beginning, do not add 'Question,Answer'."
+prompt_suffix = ""
+response_text=""
 # Loop through each page in the PDF and extract the text
 for i in range(num_pages):
     page = reader.pages[i]
     text = page.extract_text()
-    prompt += f"\n{text}"
 
-print(prompt)
-
-response = openai.Completion.create(
-            #engine="text-davinci-003",
-            model= "text-davinci-003",
+    # Add the current page text to the prompt if the resulting prompt has less than 4096 tokens
+    if len(prompt_prefix + prompt_suffix + text) < 4096:
+        prompt_suffix += f"\n{text}"
+    else:
+        # Query OpenAI API with the current prompt
+        prompt = prompt_prefix + prompt_suffix
+        response = openai.Completion.create(
+            model="text-davinci-002",
             prompt=prompt,
             max_tokens=1500,
             temperature=0.3
         )
-response_text = response['choices'][0]['text']
+        response_text += response['choices'][0]['text']
+        print(response_text)
+
+        # Reset the prompt suffix to the remaining text that didn't fit in the previous prompt
+        prompt_suffix = f"\n{text}"
+    
+# Query OpenAI API with the final prompt
+prompt = prompt_prefix + prompt_suffix
+response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1500,
+    temperature=0.3
+)
+response_text += response['choices'][0]['text']
+
 print(response_text)
+with open('response.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow([response_text])
 
 
-
-
-
+subprocess.call("./clean_csv.sh")
