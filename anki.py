a17e300f4dfe9ecbb19c12e4f17dc80404b39895
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2023-04-21 01:09:32 +0200

    original tree in master

---
+++ anki.py
@@ -0,0 +1,50 @@
+import os
+import random
+import csv
+import genanki
+
+# Read flashcards from CSV files
+flashcards_by_file = {}
+for file_name in os.listdir('out_csv'):
+    if file_name.endswith('.csv') and not file_name.endswith('.bck.csv'):
+        with open(f'out_csv/{file_name}', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            flashcards = [row for row in reader]
+        flashcards_by_file[file_name] = flashcards
+
+# Define fields and template for the Anki flashcards
+model_name = 'My Model'
+field_names = ['Question', 'Answer']
+nuumber_deck=random.randint(10**9-1,10**10 -1)
+
+my_model_template = {
+    'name': 'My Template',
+    'qfmt': '{{Question}}',
+    'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+}
+
+# Create Anki model
+my_model = genanki.Model(
+    nuumber_deck,
+    model_name,
+    fields=[
+        {'name': 'Question'},
+        {'name': 'Answer'},
+    ],
+    templates=[my_model_template],
+)
+
+# Create Anki deck for each file and add flashcards to it
+for file_name, flashcards in flashcards_by_file.items():
+    deck_name = os.path.splitext(file_name)[0]
+    my_deck = genanki.Deck(1607392319, deck_name)
+    for flashcard in flashcards:
+        my_note = genanki.Note(
+            model=my_model,
+            fields=[flashcard[0], flashcard[1]]
+        )
+        my_deck.add_note(my_note)
+    # Create Anki package and save to file
+    my_package = genanki.Package(my_deck)
+    my_package.write_to_file(f'out_anki/{deck_name}.apkg')
+
