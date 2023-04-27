2a850761e99b6e58c464d5372604a8f9184bb3fb
Cyber-Talleyrand <rdgmantuano@protonmail.com>
2023-04-27 10:01:17 +0200

    Corrected bug empty file

--- main.sh
+++ main.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #ç
+if [ -e "$(dirname "$0")/pdf_files/.gitkeep" ]; then
+    rm "$(dirname "$0")/pdf_files/.gitkeep"
+    echo "success"
+fi
 
 python3 ./pdf-ai-info-processing.py -a
 python3 ./anki.py
